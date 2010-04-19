230402f5169cea853ac351f1dd88ee3f576f08ef
peracho87 <peracho87@gmail.com>
2010-04-19 18:03:39 +0000

    Finalización clase Jugador (faltan comentarios)

--- WiPoker/src/domini/Jugador.java
+++ WiPoker/src/domini/Jugador.java
@@ -11,23 +11,31 @@ package domini;
  */
 public class Jugador {
 
-    String dni;
-    String nomComplet;
-    String alias;
-    byte edat;
-    short partidesGuanyades;
-    int puntuacio;
-    String telefon;
-    int fitxesInicials;
-    int fitxesActuals;
-    byte posicioTaula;
+    private final String dni;
+    private final String nomComplet;
+    private final String alias;
+    private final byte edat;
+    private short partidesGuanyades;
+    private int puntuacio;
+    private String telefon;
+    private int fitxesInicials;
+    private int fitxesActuals;
+    private final byte posicioTaula;
 
-    public Jugador() {
+    public Jugador(String dni, String nomComplet, String alias, byte edat, short partidesGuanyades, int puntuacio, String telefon, int fitxesInicials, int fitxesActuals, byte posicioTaula) {
+        this.dni = dni;
+        this.nomComplet = nomComplet;
+        this.alias = alias;
+        this.edat = edat;
+        this.partidesGuanyades = partidesGuanyades;
+        this.puntuacio = puntuacio;
+        this.telefon = telefon;
+        this.fitxesInicials = fitxesInicials;
+        this.fitxesActuals = fitxesActuals;
+        this.posicioTaula = posicioTaula;
     }
 
-    public Jugador(String dni, String nomComplet, String alias, byte edat, String telefon, byte posicioTaula,
-            short partidesGuanyades, int puntuacio, int fitxesInicials, int fitxesActuals) {
-        this(partidesGuanyades, puntuacio, fitxesInicials, fitxesActuals);
+    public Jugador(String dni, String nomComplet, String alias, byte edat, String telefon, byte posicioTaula) {
         this.dni = dni;
         this.nomComplet = nomComplet;
         this.alias = alias;
@@ -36,36 +44,17 @@ public class Jugador {
         this.posicioTaula = posicioTaula;
     }
 
-    public Jugador(short partidesGuanyades, int puntuacio, int fitxesInicials, int fitxesActuals) {
-        this.partidesGuanyades = partidesGuanyades;
-        this.puntuacio = puntuacio;
-        this.fitxesInicials = fitxesInicials;
-        this.fitxesActuals = fitxesActuals;
-    }
-
     public String getAlias() {
         return alias;
     }
-
-    public void setAlias(String alias) {
-        this.alias = alias;
-    }
-
+    
     public String getDni() {
         return dni;
     }
 
-    public void setDni(String dni) {
-        this.dni = dni;
-    }
-
     public byte getEdat() {
         return edat;
-    }
-
-    public void setEdat(byte edat) {
-        this.edat = edat;
-    }
+    }    
 
     public int getFitxesActuals() {
         return fitxesActuals;
@@ -86,11 +75,7 @@ public class Jugador {
     public String getNomComplet() {
         return nomComplet;
     }
-
-    public void setNomComplet(String nomComplet) {
-        this.nomComplet = nomComplet;
-    }
-
+    
     public short getPartidesGuanyades() {
         return partidesGuanyades;
     }
@@ -101,11 +86,7 @@ public class Jugador {
 
     public byte getPosicioTaula() {
         return posicioTaula;
-    }
-
-    public void setPosicioTaula(byte posicioTaula) {
-        this.posicioTaula = posicioTaula;
-    }
+    }    
 
     public int getPuntuacio() {
         return puntuacio;
@@ -141,13 +122,13 @@ public class Jugador {
     @Override
     public int hashCode() {
         int hash = 7;
-        hash = 19 * hash + (this.dni != null ? this.dni.hashCode() : 0);
+        hash = 31 * hash + (this.dni != null ? this.dni.hashCode() : 0);
         return hash;
     }
-
+    
     @Override
     public String toString () {
-        String str = nomComplet + " amb edat " + edat + " i puntuació "  + puntuacio;
+        String str = nomComplet + " amb " + edat + " anys i puntuació "  + puntuacio + "punts";
         return str;
     }
 }
