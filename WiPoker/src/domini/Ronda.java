f140d987c9241f37f508443ceab335c12cc071ac
absolut.vodk4 <absolut.vodk4@gmail.com>
2010-04-19 17:25:46 +0000

    prova

---
+++ WiPoker/src/domini/Ronda.java
@@ -0,0 +1,45 @@
+package domini;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ula
+ */
+public class Ronda {
+
+    Jugador jugadorGuanyador;
+    double pot;
+
+    public Ronda() {
+    }
+
+    public Ronda(Jugador jugadorGuanyador, double pot) {
+        this.jugadorGuanyador = jugadorGuanyador;
+        this.pot = pot;
+    }
+
+    public Jugador getJugadorGuanyador() {
+        return jugadorGuanyador;
+    }
+
+    public void setJugadorGuanyador(Jugador jugadorGuanyador) {
+        this.jugadorGuanyador = jugadorGuanyador;
+    }
+
+    public double getPot() {
+        return pot;
+    }
+
+    public void setPot(double pot) {
+        this.pot = pot;
+    }
+
+    @Override
+    public String toString() {
+        return ("Pot : " + pot  + ", guanyador: " + jugadorGuanyador.nomComplet);
+    }
+}
