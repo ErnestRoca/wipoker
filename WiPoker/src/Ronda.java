cdd4800dadb494fbe8b9c07dce49af61f5ee8ecf
absolut.vodk4 <absolut.vodk4@235ff17e-aac8-cab6-02b6-b16c8957d733>
2010-04-19 16:14:22 +0000

    prova

---
+++ WiPoker/src/Ronda.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author ula
+ */
+public class Ronda {
+    Jugador jugadorGuanyador;
+    double pot;
+
+    public Ronda() {
+
+    }
+
+    public Ronda(Jugador jugadorGuanyador, double pot) {
+        this.jugadorGuanyador = jugadorGuanyador;
+        this.pot = pot;
+    }
+
+    public Jugador getJugadorGuanyador() {
+        return jugadorGuanyador;
+    }
+
+    public void setJugadorGuanyador(Jugador jugadorGuanyador) {
+        this.jugadorGuanyador = jugadorGuanyador;
+    }
+
+    public double getPot() {
+        return pot;
+    }
+
+    public void setPot(double pot) {
+        this.pot = pot;
+    }
+
+
+}
