34066bac94e293231d03ecf1c349c082e34597a6
absolut.vodk4 <absolut.vodk4@235ff17e-aac8-cab6-02b6-b16c8957d733>
2010-04-19 17:25:59 +0000

    prova

---
+++ WiPoker/src/domini/Fase.java
@@ -0,0 +1,35 @@
+package domini;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ula
+ */
+public class Fase {
+
+    String nomFase;
+
+    public Fase() {
+    }
+
+    public Fase(String nomFase) {
+        this.nomFase = nomFase;
+    }
+
+    public String getNomFase() {
+        return nomFase;
+    }
+
+    public void setNomFase(String nomFase) {
+        this.nomFase = nomFase;
+    }
+
+    @Override
+    public String toString() {
+        return ("Fase: " + nomFase);
+    }
+}
