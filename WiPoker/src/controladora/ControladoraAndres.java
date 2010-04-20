df252f94c234f14d756a0c3ffb6112f66da7e59d
peracho87 <peracho87@235ff17e-aac8-cab6-02b6-b16c8957d733>
2010-04-20 12:42:30 +0000

    Controladora per a jugador, aposta i taula

--- WiPoker/src/controladora/ControladoraAndres.java
+++ WiPoker/src/controladora/ControladoraAndres.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package controladora;
 
 import domini.Aposta;
@@ -10,7 +9,6 @@ import domini.Baralla;
 import domini.Partida;
 import domini.Taula;
 import java.util.Calendar;
-import java.util.TimeZone;
 
 /**
  *
@@ -23,14 +21,31 @@ public class ControladoraAndres {
         return novaAposta.getQuantitat();
     }
 
+    /* Mètode que assigna la data (d'inici de la partida en estar la taula completa
+     * Paràmetres: Taula l'objecte taula en que es jugara la partida
+     * Retorna: void
+     */
     public void assignarDataPartida(Taula taula) {
         Calendar c = Calendar.getInstance();
         String data = Calendar.YEAR + "/" + Calendar.MONTH + "/" + Calendar.DAY_OF_MONTH;
         taula.setPartidaActual(new Partida(c));
     }
 
-   public void barallar(Baralla b) {
-       b.barallar();
-   }
+    /* Mètode que remena les cartes de la baralla que s'esta emprant a la partida
+     * Paràmetres: Baralla l'objecte baralla assignat a la taula
+     * Retorna: void no
+     */
+    public void barallar(Baralla b) {
+        b.barallar();
+    }
 
+    /* Mètode que controla que quedin places per afegir-se a la partida
+     * Paràmetres: Taula l'objecte taula en que es juga la partida
+     * Retorna: boolean true si la taula està al complert
+     */
+
+    public boolean taulaIsFull(Taula taula) {
+        boolean completa = taula.getPlaces() - taula.getCadiresOcupades() == 0;
+        return completa;
+    }
 }
