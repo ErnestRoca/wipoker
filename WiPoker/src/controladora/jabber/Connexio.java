a662438d1f356637707fc09e993a608b467a7991
peracho87 <peracho87@gmail.com>
2010-06-08 18:55:24 +0000

    Canvis per a agrupar codi i reduir redundancies als metodes que gestionen les fases

--- WiPoker/src/controladora/jabber/Connexio.java
+++ WiPoker/src/controladora/jabber/Connexio.java
@@ -8,6 +8,8 @@ package controladora.jabber;
 
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smackx.Form;
+import org.jivesoftware.smackx.muc.MultiUserChat;
 
 /**
  *
@@ -18,6 +20,29 @@ public class Connexio {
     public static XMPPConnection crearConnexio(String servidor) throws XMPPException {       
         XMPPConnection connexio = new XMPPConnection(servidor, 5222);       
         return connexio;
+    }
+
+    public static MultiUserChat crearSala(JID jid, XMPPConnection con) {
+        //cadena completa per servidor jabberes.org sala@conf.jabberes.org/andres
+        
+        MultiUserChat muc = new MultiUserChat(con, jid.getFullJID());
+        try {
+            muc.create(jid.name);
+            muc.sendConfigurationForm(new Form(Form.TYPE_SUBMIT));
+            muc.changeSubject("sala per al joc wipoker");
+        } catch (Exception e) {
+            System.out.println("error creant sala");
+            System.out.println(e.getMessage());
+        }
+        try {
+            if (!muc.isJoined()) {
+                muc.join(jid.getNick());
+            }
+        } catch (Exception e) {
+            System.out.println("error entrant sala");
+        }
+        
+        return muc;
     }
 }
 
