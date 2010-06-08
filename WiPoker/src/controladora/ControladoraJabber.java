f44e33f099838d74abad3bcfd80ee781856ab958
peracho87 <peracho87@gmail.com>
2010-06-08 18:55:46 +0000

    Canvis per a agrupar codi i reduir redundancies als metodes que gestionen les fases

--- WiPoker/src/controladora/ControladoraJabber.java
+++ WiPoker/src/controladora/ControladoraJabber.java
@@ -33,7 +33,6 @@ public class ControladoraJabber {
     private JID room;
     private Listeners listeners;
     private Trafic trafic;
-    
 
     public ControladoraJabber() {
         listeners = new Listeners();
@@ -56,45 +55,12 @@ public class ControladoraJabber {
     public void setRoom(JID room) {
         this.room = room;
     }
-    
+
     public MultiUserChat getMuc() {
         return muc;
     }
 
     public Listeners getListeners() {
         return listeners;
-    }
-    
-    public void crearSala(JID jid) {
-        //cadena completa per servidor jabberes.org sala@conf.jabberes.org/andres
-        room = jid;
-        muc = new MultiUserChat(connexio, jid.getFullJID());
-        try {
-            muc.create(room.getNick());
-            muc.sendConfigurationForm(new Form(Form.TYPE_SUBMIT));
-            muc.changeSubject("sala per al joc wipoker");
-        } catch (Exception e) {
-            System.out.println("error creant sala");
-            System.out.println(e.getMessage());
-        }
-        try {
-            if (!muc.isJoined()) {
-                muc.join(room.getNick());
-            }
-        } catch (Exception e) {
-            System.out.println("error entrant sala");
-        }
-        if (muc != null) {
-            try {
-                muc.addParticipantStatusListener(listeners);
-                muc.addUserStatusListener(listeners);
-                PacketFilter pf = new AndFilter(new PacketTypeFilter(muc.createMessage().getClass()),
-                        new FromContainsFilter(muc.getRoom()));
-                connexio.createPacketCollector(pf);
-                connexio.addPacketListener(listeners, pf);
-            } catch (Exception e) {
-                System.out.println("error afegint escoltadors");
-            }
-        }
     }
 }
