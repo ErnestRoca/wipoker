18c23951e54cd361c162b1a9e6cee35d903113b1
Ernest <ernst.roca@gmail.com>
2010-06-15 17:20:45 +0000

    WiPoker: Implementació de l'aplicació

--- WiPoker/src/domini/Bot.java
+++ WiPoker/src/domini/Bot.java
@@ -1,37 +1,44 @@
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+
+/**
+ * Bot.java
  */
+
 package domini;
 
 import controladora.ControladoraIA;
-import javax.swing.ImageIcon;
 
 /**
+ * Descripció de la classe concreta Persona.<br>
+ * Modela un Bot(jugador automàtic) en el domini suggerit pels requeriments funcionals
  *
  * @author ula
  */
 public class Bot extends Jugador {
 
-    private ImageIcon avatar;
-
+    /**
+     * Constructor d'instàncies de la classe.<br>
+     * pre: Les indicades als paràmetres<br>
+     * post: S'ha creat una instància de la classe actual i s'han assignat els valors
+     * dels paràmetres als atributs<br>
+     * 
+     * @param alias
+     * @param fitxesInicials
+     * @param posicioTaula
+     */
     public Bot(String alias, int fitxesInicials, int posicioTaula) {
         super(alias, fitxesInicials, posicioTaula, "bot");
     }
 
+    /**
+     * Determina què ha de fer el bot, quin tipus de jugada
+     * @param ci
+     * @param fase
+     * @param numVegadesFase
+     */
     public void jugadaBot(ControladoraIA ci, Fase fase, int numVegadesFase) {
         ci.calcularAposta(this, fase, numVegadesFase);
-    }
 
-//    @Override
-//    public Aposta getAposta() {
-//        return aposta;
-//    }
-//
-//    @Override
-//    public void setAposta(Aposta aposta) {
-//        this.aposta = aposta;
-//    }
+    }
 
     @Override
     public String toString() {
