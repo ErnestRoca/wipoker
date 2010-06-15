da040b101e29bfb7c33e1e6b7ec94732cd1332e4
ernst.roca <ernst.roca@235ff17e-aac8-cab6-02b6-b16c8957d733>
2010-06-15 17:12:13 +0000

    WiPoker: Implementació de l'aplicació

--- WiPoker/src/domini/Baralla.java
+++ WiPoker/src/domini/Baralla.java
@@ -1,37 +1,77 @@
+/**
+ * Baralla.java
+ */
 package domini;
 
 import java.util.ArrayList;
 
 /**
- *
+ * Descripció de la classe concreta Persona.<br>
+ * Modela una Baralla en el domini suggerit pels requeriments funcionals
+ * 
  * @author ernest
  */
 public class Baralla {
 
-    private ArrayList<Carta> Cartes = new ArrayList<Carta>();
-    private static final byte maxCartes = 52;         //Nombre total de cartes de que estarà formada la Baralla.
-    private byte cartesActuals;     //Nombre de cartes que queden per descartar.
+    // Atributs, variables membre o camps: Modelen les dades que la classe i les seves instàncies han de saber o conèixer en tot moment
+    
+    private static final byte maxCartes = 52;       //Nombre total de cartes de que estarà formada la Baralla.
+    private byte cartesActuals;                     //Nombre de cartes que queden per descartar.
 
+    // Pseudoatributs per establir i mantenir connexions permanents entre la instància actual i altres instàncies
+
+    private ArrayList<Carta> Cartes = new ArrayList<Carta>();
+
+    /**
+     * Constructor d'instàncies de la classe.<br>
+     * pre: Les indicades als paràmetres<br>
+     * post: S'ha creat una instància de la classe actual i s'han assignat els valors
+     * dels paràmetres als atributs<br>
+     * 
+     */
     public Baralla() {
         this.cartesActuals = maxCartes;
     }
 
+    /**
+     * Retorna el valor de l'atribut Cartes.
+     * @return les cartes que formen la baralla
+     */
     public ArrayList<Carta> getCartes() {
         return Cartes;
     }
 
+    /**
+     * El nou valor de Cartes
+     * @param Cartes
+     */
     public void setCartes(ArrayList<Carta> Cartes) {
         this.Cartes = Cartes;
     }
 
+    /**
+     * Retorna el valor de l'atribut Cartes.
+     * @return Nombre de cartes que queden per descartar
+     */
     public byte getCartesActuals() {
         return cartesActuals;
     }
 
+    /**
+     * El nou valor de cartesActuals
+     * @param Nombre de cartes que queden per descartar
+     */
     public void setCartesActuals(int cartesActuals) {
         this.cartesActuals = (byte) cartesActuals;
     }
 
+    /**
+     * Compara la instància actual amb una referència a una altra instància per determinar-ne la seva "igualtat".<br>
+     * pre: Les indicades als paràmetres<br>
+     * post: Les indicades al return<br>
+     * @param obj Una referència a una instància Object
+     * @return true si considerem ambdós instàncies "iguals" (mateix objecte a nivell del domini)
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
