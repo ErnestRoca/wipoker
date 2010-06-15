976b631a2d4ae279718f5e65bea4167d5e225c45
ernst.roca <ernst.roca@235ff17e-aac8-cab6-02b6-b16c8957d733>
2010-06-15 16:16:30 +0000

    WiPoker: Implementació de l'aplicació

--- WiPoker/src/domini/Jugador.java
+++ WiPoker/src/domini/Jugador.java
@@ -1,13 +1,12 @@
+/*
+ * Jugador.java
+ */
 package domini;
 
 import controladora.Torn;
 import java.util.ArrayList;
 import javax.swing.ImageIcon;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author Andrés
@@ -76,120 +75,44 @@ public class Jugador {
         }
     }
 
+    /**
+     * Retorna el valor de l'atribut alies.
+     * @return alies del jugador
+     */
     public String getAlias() {
         return alias;
     }
 
+    /**
+     * Retorna el valor de l'atribut dni.
+     * @return dni del jugador
+     */
     public String getDni() {
         return dni;
     }
 
+    /**
+     * Retorna el valor de l'atribut edat.
+     * @return edat del jugador
+     */
     public byte getEdat() {
         return edat;
     }
 
-    public int getFitxesActuals() {
-        return fitxesActuals;
-    }
-
-    public void setFitxesActuals(int fitxesActuals) {
-        this.fitxesActuals = fitxesActuals;
-    }
-
-    public int getFitxesInicials() {
-        return fitxesInicials;
-    }
-
-    public void setFitxesInicials(int fitxesInicials) {
-        this.fitxesInicials = fitxesInicials;
-    }
-
+    /**
+     * Retorna el valor de l'atribut nomComplet.
+     * @return nomComplet del jugador
+     */
     public String getNomComplet() {
         return nomComplet;
     }
 
-    public short getPartidesGuanyades() {
-        return partidesGuanyades;
-    }
-
-    public void setPartidesGuanyades(int partidesGuanyades) {
-        this.partidesGuanyades = (short) partidesGuanyades;
-    }
-
+    /**
+     * Retorna el valor de l'atribut posicioTaula.
+     * @return posicio del jugador la taula
+     */
     public byte getPosicioTaula() {
         return posicioTaula;
-    }
-
-    public int getPuntuacio() {
-        return puntuacio;
-    }
-
-    public void setPuntuacio(int puntuacio) {
-        this.puntuacio = puntuacio;
-    }
-
-    public String getTelefon() {
-        return telefon;
-    }
-
-    public void setTelefon(String telefon) {
-        this.telefon = telefon;
-    }
-
-    public Ma getMaActual() {
-        return maActual;
-    }
-
-    public void setMaActual(Ma maActual) {
-        this.maActual = maActual;
-    }
-
-    public Aposta getAposta() {
-        return aposta;
-    }
-
-    public void setAposta(Aposta aposta) {
-        this.aposta = aposta;
-    }
-
-    public int getApostaTotalRonda() {
-        return apostaTotalRonda;
-    }
-
-    public void setApostaTotalRonda(int apostaTotalRonda) {
-        this.apostaTotalRonda = apostaTotalRonda;
-    }
-
-    public boolean isHaFetFold() {
-        return haFetFold;
-    }
-
-    public void setHaFetFold(boolean haFetFold) {
-        this.haFetFold = haFetFold;
-    }
-
-    public boolean isAllin() {
-        return haFetAllin;
-    }
-
-    public void setHaFetAllin(boolean haFetAllin) {
-        this.haFetAllin = haFetAllin;
-    }
-
-    public boolean isEliminat() {
-        return eliminat;
-    }
-
-    public void setEliminat(boolean eliminat) {
-        this.eliminat = eliminat;
-    }
-
-    public Torn getTorn() {
-        return torn;
-    }
-
-    public void setTorn(Torn torn) {
-        this.torn = torn;
     }
 
     /**
@@ -202,6 +125,150 @@ public class Jugador {
 
     public void setAvatar(ImageIcon avatar) {
         this.avatar = avatar;
+    }
+
+    /**
+     * Retorna el valor de l'atribut aposta.
+     * @return aposta feta pel jugador
+     */
+    public Aposta getAposta() {
+        return aposta;
+    }
+
+    public void setAposta(Aposta aposta) {
+        this.aposta = aposta;
+    }
+
+    /**
+     * Retorna el valor de l'atribut apostaTotalRonda.
+     * @return la quantitat total d'apostes en una ronda
+     */
+    public int getApostaTotalRonda() {
+        return apostaTotalRonda;
+    }
+
+    public void setApostaTotalRonda(int apostaTotalRonda) {
+        this.apostaTotalRonda = apostaTotalRonda;
+    }
+
+    /**
+     * Retorna el valor de l'atribut eliminat.
+     * @return true, si el jugador s'ha eliminat
+     */
+    public boolean isEliminat() {
+        return eliminat;
+    }
+
+    public void setEliminat(boolean eliminat) {
+        this.eliminat = eliminat;
+    }
+
+    /**
+     * Retorna el valor de l'atribut fitxesActuals.
+     * @return la quantitat de fitxes de que disposa en aquest moment
+     */
+    public int getFitxesActuals() {
+        return fitxesActuals;
+    }
+
+    public void setFitxesActuals(int fitxesActuals) {
+        this.fitxesActuals = fitxesActuals;
+    }
+
+    /**
+     * Retorna el valor de l'atribut fitxesInicials.
+     * @return quantitat de fitxes amb les que el jugador comença la partida
+     */
+    public int getFitxesInicials() {
+        return fitxesInicials;
+    }
+
+    public void setFitxesInicials(int fitxesInicials) {
+        this.fitxesInicials = fitxesInicials;
+    }
+
+    /**
+     * Retorna el valor de l'atribut haFetAllin.
+     * @return imatge associada a la pesa
+     */
+    public boolean HaFetAllin() {
+        return haFetAllin;
+    }
+
+    public void setHaFetAllin(boolean haFetAllin) {
+        this.haFetAllin = haFetAllin;
+    }
+
+    /**
+     * Retorna el valor de l'atribut haFetFold.
+     * @return imatge associada a la pesa
+     */
+    public boolean isHaFetFold() {
+        return haFetFold;
+    }
+
+    public void setHaFetFold(boolean haFetFold) {
+        this.haFetFold = haFetFold;
+    }
+
+    /**
+     * Retorna el valor de l'atribut imatge.
+     * @return imatge associada a la pesa
+     */
+    public Ma getMaActual() {
+        return maActual;
+    }
+
+    public void setMaActual(Ma maActual) {
+        this.maActual = maActual;
+    }
+
+    /**
+     * Retorna el valor de l'atribut imatge.
+     * @return imatge associada a la pesa
+     */
+    public short getPartidesGuanyades() {
+        return partidesGuanyades;
+    }
+
+    public void setPartidesGuanyades(short partidesGuanyades) {
+        this.partidesGuanyades = partidesGuanyades;
+    }
+
+    /**
+     * Retorna el valor de l'atribut imatge.
+     * @return imatge associada a la pesa
+     */
+    public int getPuntuacio() {
+        return puntuacio;
+    }
+
+    public void setPuntuacio(int puntuacio) {
+        this.puntuacio = puntuacio;
+    }
+
+    /**
+     * Retorna el valor de l'atribut imatge.
+     * @return imatge associada a la pesa
+     */
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    /**
+     * Retorna el valor de l'atribut imatge.
+     * @return imatge associada a la pesa
+     */
+    public Torn getTorn() {
+        return torn;
+    }
+
+    public void setTorn(Torn torn) {
+        this.torn = torn;
     }
 
     @Override
